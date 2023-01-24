cc0f2bf6db2336929440392e95b90ca08c1883fb
Saud0227 <saud02tech@gmail.com>
2023-01-24 23:17:26 +0100

    docker testing, stuff works ish

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -60,3 +62,8 @@ def update(id):
             return 'update db failure'
     else:
         return render_template('update.html', task=task)
+
+#
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(debug=False, host='0.0.0.0', port=port)
